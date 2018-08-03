0fb2ae80059864e2646e592dd9a0061e5bca2b14
Nikkely <umivudau153@gmail.com>
2018-08-04 00:23:06 +0900

    finish narou

--- scrape.rb
+++ scrape.rb
@@ -2,17 +2,22 @@ require 'nokogiri'
 require 'open-uri'
 require 'json'
 
-apiUrl = "https://api.syosetu.com/novel18api/api/?lim=20\&out=json\&word=#{URI.encode("エロ")}\&of=n-ga"
+#変更部分
+query = "lim=20\&out=json\&word=#{URI.encode("エロ")}\&order=lengthasc"
+sleepSec = 1 #小数も設定可能
+useTitle = false #trueならディレクトリ名が小説タイトルに
+#変更部分終わり
+apiUrl = "https://api.syosetu.com/novelapi/api/?of=n-ga-t\&#{query}"
 
-def openAndWrite(ncode, index)
+def openAndWrite(ncode, index, title)
   charset = nil
   html = open('https://ncode.syosetu.com/' + ncode + '/' + index) do | page |
     charset = page.charset
     page.read
   end
-
+  p 'https://ncode.syosetu.com/' + ncode + '/' + index
   doc = Nokogiri::HTML.parse(html, nil, charset)
-  File.open("dl/#{ncode}/#{index}p.txt", "a") do | textfile |
+  File.open("dl/#{title}/#{index}p.txt", "a") do | textfile |
     textfile.puts(doc.xpath('//p[@class="novel_subtitle"]').inner_text)
     textfile.puts(doc.xpath('//div[@id="novel_honbun"]').inner_text)
   end
@@ -24,17 +29,18 @@ hash.each do |row|
   if !row['ncode'].nil?
     ncode = row['ncode'].downcase
     num = row['general_all_no']
-    Dir.mkdir('dl/' + ncode, 0755) unless FileTest.exist?('dl/' + ncode)
+    title = useTitle ? row['title'] : row['ncode']
+    Dir.mkdir('dl/' + title, 0755) unless FileTest.exist?('dl/' + title)
     if num > 1
       num.times do |i|
-        openAndWrite(ncode, (i+1).to_s)
-        sleep(1)
-        p "complete #{ncode} : #{num}/#{i+1}"
+        openAndWrite(ncode, (i+1).to_s, title)
+        sleep(sleepSec)
+        p "complete #{ncode}(#{title}) : #{num}/#{i+1}"
       end
     else
-      openAndWrite(ncode, "")
-      sleep(1)
-      p "complete #{ncode}"
+      openAndWrite(ncode, "", title)
+      sleep(sleepSec)
+      p "complete #{ncode}(#{title})"
     end
   end
 end
